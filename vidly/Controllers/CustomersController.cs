9f1295e5d97abedcd1d7f7a6695c4e8ec13d83ff
Nizam <nizam.icup@gmail.com>
2016-07-13 14:22:18 +0200

    create code first database using EF and added migrations, added customer to database

--- vidly/Controllers/CustomersController.cs
+++ vidly/Controllers/CustomersController.cs
@@ -2,33 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 using System.Web.Mvc;
 using vidly.Models;
-using vidly.ViewModels;
+
 
 namespace vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = new List<Customer>()
-        {
-            new Customer {Id = 1 ,Name = "customer 1"},
-            new Customer {Id = 2 ,Name = "customer 2"}
-        };
-            var viewmodel = new RandomMovieViewModel
-            {
-                Customers = customers
-            };
-            return View(viewmodel);
+            var customers = _context.Customers.Include(c => c.MebershipType).ToList();
+            return View(customers);
         }
         public ActionResult Details(int id)
         {
-           
-            return View();
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            return View(customer);
         }
     }
 }
--- vidly/Models/IdentityModels.cs
+++ vidly/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace vidly.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -29,5 +30,7 @@ namespace vidly.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<vidly.Models.MembershipType> MembershipTypes { get; set; }
     }
 }
